c62024493b936f54ba92e40cf87f3cc9c4aad01b
shrey-Odinproject <sonishreyf1@gmail.com>
2022-01-27 22:05:10 +0530

    created a Display module

---
+++ lib/display.rb
@@ -0,0 +1,25 @@
+module Display
+  def display_intro
+    'starting new game of connect four ...'
+  end
+
+  def display_ask_player_name(num)
+    "player #{num} enter your name"
+  end
+
+  def display_incorrect_input
+    'Erronous input ! try again'
+  end
+
+  def display_ask_input(name,symbol)
+    "#{name} which column to place #{symbol} in ?"
+  end
+
+  def display_win_message(name)
+    " Game Over ! #{name} has won "
+  end
+
+  def display_draw_message
+    'Game over its a DRAW'
+  end
+end
